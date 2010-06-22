2a8d299a3243a7f350b2f107707fc5c0732432d4
Mike Chan <mike@android.com>
2010-06-22 13:26:45 -0500

    cpufreq: interactive: New 'interactive' governor
    New interactive governor.
    
    This governor is designed for latency sensitive workloads, UI interaction for
    example.
    
    Advantages:
    + significantly more responsive to ramp cpu up when required (UI interaction)
    + more consistent ramping, existing governors do their cpu load sampling in a
    workqueue context, the 'interactive' governor does this in a timer context, which
    gives more consistent cpu load sampling.
    + higher priority for cpu frequency increase, rt_workqueue is used for scaling
    up, giving the remaining tasks the cpu performance benefit, unlike existing
    governors which schedule rampup work to occur after your performance starved
    tasks have completed.
    
    Existing governors sample cpu load at a particular rate, typically
    every X ms. Which can lead to under powering UI threads when the user has
    interacted with an idle system until the next sample period happns.
    
    The 'interactive' governor has a different approach. Instead of sampling the cpu
    at a specified rate, the governor will scale the cpu frequency up when coming
    out of idle. When the cpu comes out of idle, a timer is configured to fire
    within 1-2 ticks. If the cpu is 100% busy from exiting idle to when the timer
    fires then we assume the cpu is underpowered and ramp to MAX speed.
    
    If the cpu was not 100% busy, then the governor evaluates the cpu load over the
    last 'min_sample_rate' (default 50000 uS) to determine the cpu speed to ramp down
    to.
    
    There is only one tuneable for this governor:
    /sys/devices/system/cpu/cpufreq/interactive/min_sample_rate:
    	The minimum ammount of time to spend at the current frequency before
    	ramping down. This is to ensure that the governor has seen enough
    	historic cpu load data to determine the appropriate workload.
    	Default is 5000 uS.
    
    Signed-off-by: Mike Chan <mike@android.com>
    Change-Id: I686d2f57b0ed9cbb73217403b7438be5719588d2
    
    cpufreq: interactive: Fix potential division by zero
    
    cpufreq: interactive: Changes to interactive governor
    
    Changes include:
    * May scale up to intermediate speeds after scaling down, rather than
    scale to max speed and then only scale down until max speed needed.
    
    * Tweaked thresholds at which max speed requested (previously CPU must
    have been 100% busy since idle exit timer started, now will go max if at
    least 85% busy) and default minimum sample time raised to 80ms. Tweaking
    based on UI tests, still in progress.
    
    * SMP fixes.
    
    * Fixed attempted multiple delete of sysfs group on governor stop.
    Set a just-in-case-CPU-goes-busy-again timer even if nr_running == 0 at
    timer function run time, but cancel if that CPU goes idle (and don't
    re-arm timer if that CPU is currently idle).
    
    * Re-evaluate speed if a CPU goes idle while above min speed (and no timer
    currently set) in case the platform requires all CPUs to be at the same
    speed.
    
    * Realtime workqueues disappeared upstream, convert speed up workqueue to
    a realtime task. Average scheduling latency measured significantly less
    than WQ_HIGHPRI.
    
    * Timers are not deferrable, must wake CPU from idle, since we now
    re-evaluate speed for idle CPUs.
    
    * CPU load is computed from higher of short-term load since idle exit vs.
    long-term load since last frequency change, to avoid dropping speed
    during temporary dips in load on long-term-busy CPU.
    
    * Avoid 1 CPU starting new idle exit load eval interval in a race with
    timer running on another CPU.
    
    * New fugly debugging printfs should be reworked or go away eventually.
    
    Change-Id: I606b5c1850637c35a7814309df12362d5c044825
    via: https://review.source.android.com//#change,15809

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -343,7 +343,11 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
+<<<<<<< HEAD
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_interactive)
+=======
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+>>>>>>> df57591... cpufreq: interactive: New 'interactive' governor
 #endif
 
 
