964fb7332c5426b7ec29a7632851197598455d47
Steve Kondik <shade@chemlab.org>
2010-11-11 19:01:05 -0500

    bcm4329: Use the latest driver from Broadcom/Google.
     * Version 4.218.248-15

--- arch/arm/mach-msm/board-spade-wifi.c
+++ arch/arm/mach-msm/board-spade-wifi.c
@@ -1,5 +1,17 @@
-/* linux/arch/arm/mach-msm/board-vision-wifi.c
-*/
+/* linux/arch/arm/mach-msm/board-spade-wifi.c
+ *
+ * Copyright (C) 2010-2011 HTC Corporation.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
@@ -11,11 +23,11 @@
 #include <linux/skbuff.h>
 #include <linux/wlan_plat.h>
 
-#include "board-vision.h"
+#include "board-spade.h"
 
-int vision_wifi_power(int on);
-int vision_wifi_reset(int on);
-int vision_wifi_set_carddetect(int on);
+int spade_wifi_power(int on);
+int spade_wifi_reset(int on);
+int spade_wifi_set_carddetect(int on);
 
 #define PREALLOC_WLAN_NUMBER_OF_SECTIONS	4
 #define PREALLOC_WLAN_NUMBER_OF_BUFFERS		160
@@ -42,7 +54,7 @@ static wifi_mem_prealloc_t wifi_mem_array[PREALLOC_WLAN_NUMBER_OF_SECTIONS] = {
 	{ NULL, (WLAN_SECTION_SIZE_3 + PREALLOC_WLAN_SECTION_HEADER) }
 };
 
-static void *vision_wifi_mem_prealloc(int section, unsigned long size)
+static void *spade_wifi_mem_prealloc(int section, unsigned long size)
 {
 	if (section == PREALLOC_WLAN_NUMBER_OF_SECTIONS)
 		return wlan_static_skb;
@@ -53,17 +65,17 @@ static void *vision_wifi_mem_prealloc(int section, unsigned long size)
 	return wifi_mem_array[section].mem_ptr;
 }
 
-int __init vision_init_wifi_mem(void)
+int __init spade_init_wifi_mem(void)
 {
 	int i;
 
-	for (i = 0; (i < WLAN_SKB_BUF_NUM); i++) {
+	for(i=0;( i < WLAN_SKB_BUF_NUM );i++) {
 		if (i < (WLAN_SKB_BUF_NUM/2))
 			wlan_static_skb[i] = dev_alloc_skb(4096);
 		else
 			wlan_static_skb[i] = dev_alloc_skb(8192);
 	}
-	for (i = 0; (i < PREALLOC_WLAN_NUMBER_OF_SECTIONS); i++) {
+	for(i=0;( i < PREALLOC_WLAN_NUMBER_OF_SECTIONS );i++) {
 		wifi_mem_array[i].mem_ptr = kmalloc(wifi_mem_array[i].size,
 							GFP_KERNEL);
 		if (wifi_mem_array[i].mem_ptr == NULL)
@@ -72,35 +84,35 @@ int __init vision_init_wifi_mem(void)
 	return 0;
 }
 
-static struct resource vision_wifi_resources[] = {
+static struct resource spade_wifi_resources[] = {
 	[0] = {
 		.name		= "bcm4329_wlan_irq",
-		.start		= MSM_GPIO_TO_INT(VISION_GPIO_WIFI_IRQ),
-		.end		= MSM_GPIO_TO_INT(VISION_GPIO_WIFI_IRQ),
+		.start		= MSM_GPIO_TO_INT(SPADE_GPIO_WIFI_IRQ),
+		.end		= MSM_GPIO_TO_INT(SPADE_GPIO_WIFI_IRQ),
 		.flags          = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL | IORESOURCE_IRQ_SHAREABLE,
 	},
 };
 
-static struct wifi_platform_data vision_wifi_control = {
-	.set_power      = vision_wifi_power,
-	.set_reset      = vision_wifi_reset,
-	.set_carddetect = vision_wifi_set_carddetect,
-	.mem_prealloc   = vision_wifi_mem_prealloc,
+static struct wifi_platform_data spade_wifi_control = {
+	.set_power      = spade_wifi_power,
+	.set_reset      = spade_wifi_reset,
+	.set_carddetect = spade_wifi_set_carddetect,
+	.mem_prealloc   = spade_wifi_mem_prealloc,
 };
 
-static struct platform_device vision_wifi_device = {
-	.name           = "bcm4329_wlan",
-	.id             = 1,
-	.num_resources  = ARRAY_SIZE(vision_wifi_resources),
-	.resource       = vision_wifi_resources,
-	.dev            = {
-		.platform_data = &vision_wifi_control,
-	},
+static struct platform_device spade_wifi_device = {
+        .name           = "bcm4329_wlan",
+        .id             = 1,
+        .num_resources  = ARRAY_SIZE(spade_wifi_resources),
+        .resource       = spade_wifi_resources,
+        .dev            = {
+                .platform_data = &spade_wifi_control,
+        },
 };
 
 extern unsigned char *get_wifi_nvs_ram(void);
 
-static unsigned vision_wifi_update_nvs(char *str)
+static unsigned spade_wifi_update_nvs(char *str)
 {
 #define NVS_LEN_OFFSET		0x0C
 #define NVS_DATA_OFFSET		0x40
@@ -114,7 +126,7 @@ static unsigned vision_wifi_update_nvs(char *str)
 	memcpy(&len, ptr + NVS_LEN_OFFSET, sizeof(len));
 
 	/* the last bye in NVRAM is 0, trim it */
-	if (ptr[NVS_DATA_OFFSET + len - 1] == 0)
+	if (ptr[NVS_DATA_OFFSET + len -1] == 0)
 		len -= 1;
 
 	if (ptr[NVS_DATA_OFFSET + len -1] != '\n') {
@@ -128,16 +140,15 @@ static unsigned vision_wifi_update_nvs(char *str)
 	return 0;
 }
 
-int __init vision_wifi_init(void)
+int __init spade_wifi_init(void)
 {
 	int ret;
 
 	printk(KERN_INFO "%s: start\n", __func__);
-	vision_wifi_update_nvs("sd_oobonly=1\n");
-	vision_wifi_update_nvs("btc_params80=0\n");
-	vision_wifi_update_nvs("btc_params6=30\n");
-	vision_init_wifi_mem();
-	ret = platform_device_register(&vision_wifi_device);
-	return ret;
+	spade_wifi_update_nvs("sd_oobonly=1\n");
+	spade_wifi_update_nvs("btc_params80=0\n");
+	spade_wifi_update_nvs("btc_params6=30\n");
+	spade_init_wifi_mem();
+	ret = platform_device_register(&spade_wifi_device);
+        return ret;
 }
-
